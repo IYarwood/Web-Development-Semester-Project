f9d818a8bcf170b2a8bd78cb75b2a1b6df3a771a
clodon2 <90060114+clodon2@users.noreply.github.com>
2025-03-25 20:43:49 -0400

    Remove console logs, update manual
    I removed my testing console.logs from submitPopUp and started updating programmerManual (currently just updated for submitPopUp.js)

--- scripts/submitPopUp.js
+++ scripts/submitPopUp.js
@@ -1,36 +1,37 @@
+// start listening for submit on page full load
 function on_load(event) {
     // get form element from document
-    console.log("test");
     const form = document.querySelector('.form-container');
-    const ts = form.querySelectorAll(".form-group > *:not(:first-child)");
-    console.log(ts);
 
     form.addEventListener("submit", function(e) {
+        // prevent form from clearing on submit
         e.preventDefault();
         on_submit(e);
 });
 }
 
 
+// when submit happnes translate form data to alert
 function on_submit(event) {
     const form_data = get_form_data(event.target);
     alert(format_alert_output(form_data));
 }
 
 
+// get input data from the form and translate it to a map
 function get_form_data(form_container) {
-    console.log("contained:");
+    // get field names from label, pair with input field value from same form group
     const label_data = form_container.querySelectorAll(".form-group > *:first-child");
     const input_data = form_container.querySelectorAll(".form-group > *:not(:first-child)");
     const input_map = {};
     for (let i = 0; i < input_data.length; i++) {
-        console.log(label_data[i])
         input_map[label_data[i].textContent] = input_data[i].value;
     }
     return input_map;
 }
 
 
+// format a map of input data to an output string for the alert pop up
 function format_alert_output(form_data) {
     let output_string = "Submitted the following for each field: \n";
     for (const [input_type, input_value] of Object.entries(form_data)) {
