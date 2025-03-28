3ef0581ab670384343efa3093a8f8de1a9f2d457
IYarwood <ianzyarwood@gmail.com>
2025-03-27 20:36:30 -0400

    Got selection display working

--- scripts/submitPopUp.js
+++ scripts/submitPopUp.js
@@ -20,13 +20,30 @@ function on_submit(event) {
 */
 
 // get input data from the form and translate it to a map
-function get_form_data(form_container) {
+function get_form_data(form_container, days, time) {
     // get field names from label, pair with input field value from same form group
     const label_data = form_container.querySelectorAll(".form-group > *:first-child");
     const input_data = form_container.querySelectorAll(".form-group > *:not(:first-child)");
     const input_map = {};
+    const timeChecks = ["days", "mwfTime", "mwTime", "tthTime", "singleDayTime"];
+    const readableDays = {
+        mwf: "Monday, Wednesday, Friday",
+        mw: "Monday, Wednesday",
+        tth: "Tuesday, Thursday",
+        m: "Monday",
+        t: "Tuesday",
+        th: "Thursday"
+    };
     for (let i = 0; i < input_data.length; i++) {
-        input_map[label_data[i].textContent] = input_data[i].value;
+        if (input_data[i].tagName == "SELECT" && (timeChecks.includes(input_data[i].id))){
+            if (input_data[i].value == days){
+                input_map[label_data[i].textContent] = readableDays[input_data[i].value];
+            }else if (input_data[i].value == time){
+                input_map["Time: "] = input_data[i].options[input_data[i].selectedIndex].textContent;
+            }
+        }else{
+            input_map[label_data[i].textContent] = input_data[i].value;
+        }
     }
     return input_map;
 }
@@ -41,12 +58,9 @@ function format_alert_output(form_data) {
     return output_string
 }
 
-export function triggerPopUp(formElement) {
-    const formData = get_form_data(formElement);
+export function triggerPopUp(formElement, days, time) {
+    const formData = get_form_data(formElement, days, time);
     alert(format_alert_output(formData));
 }
 
-document.addEventListener("DOMContentLoaded", function(e) {
-    on_load(e);
-});
 
